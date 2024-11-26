ed29dd9a8e0d777b3e35821010644b7f407986cd
kethan <kethankoushik09@gmail.com>
2024-11-26 22:55:28 +0530

    app.js  updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./pages/Nav";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Cart from "./cart";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import db from "./components/firebaseinit";
+// import cutsomAuthContext from "./components/AuthContext";
+import { Customusercontext } from "./components/UserContext";
+import CustomAuthContext from "./components/AuthContext";
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import Orders from "./pages/orders";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+  const rt = createBrowserRouter([
+    {path:"/",
+      element:<Navbar />, 
+      children:[
+      {index:true, element:<Home/>},
+      {path:"signin",element:<Signin/>},
+      {path:"/signup",element:<SignUp/>},
+      {path:"cart",element:<Cart/>},
+      {path:"order",element:<Orders/>}
+
+    ]}
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Customusercontext>
+      <CustomAuthContext>
+        <RouterProvider router={rt}/>
+        <ToastContainer />
+      </CustomAuthContext>
+    </Customusercontext>
+    </>
   );
 }
 
